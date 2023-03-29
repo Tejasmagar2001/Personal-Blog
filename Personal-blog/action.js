27d1810604ca10efc36ea304000c078a845f5bfc
Tejasmagar2001 <125381265+Tejasmagar2001@users.noreply.github.com>
2023-03-29 23:58:00 +0530

    Add files via upload

---
+++ Personal-blog/action.js
@@ -0,0 +1,14 @@
+function action(){
+
+
+const a = document.getElementById('check')
+const b = document.getElementById('top-m')
+
+if(a.checked == true){
+    b.style.display='block';
+   
+} else{
+ b.style.display='none';
+ 
+}
+}
